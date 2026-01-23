a1004f50ceee58b616f9e31fca5c9436e0746852
Ruturajkumbhar <ruturajkumbhar2612@gmail.com>
2026-01-23 20:05:10 +0530

    Update backup.sh

--- Day_4/backup.sh
+++ Day_4/backup.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-log_dir="/var/log/myapp"
-backup_dir="/backup/logs"
+# Folders
+log_dir="$HOME/myapp/logs"
+backup_dir="$HOME/myapp/backup"
+log_file="$HOME/myapp/backup_log.txt"
 
+# Create folders if not exist
+mkdir -p "$log_dir"
 mkdir -p "$backup_dir"
 
-find "$log_dir" -type f -mtime +7 -exec mv {} "$backup_dir" \;
+# Start log
+echo "Backup started at: $(date)" >> "$log_file"
 
-echo " Old logs archived successfully"
+# Move files older than 7 days
+find "$log_dir" -type f -mtime +7 -print -exec mv {} "$backup_dir" \; >> "$log_file" 2>&1
 
+# End log
+echo "Backup finished at: $(date)" >> "$log_file"
+echo "----------------------------" >> "$log_file"
+
+# Message on screen
+echo "Backup done. Check log file: $log_file"
