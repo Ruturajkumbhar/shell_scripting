c08a0e9c0ad5bc80857e6a04b42f69a11a640ee7
Ruturajkumbhar <ruturajkumbhar2612@gmail.com>
2026-01-29 08:21:47 +0530

    Update disk_monitoring_cron.sh

--- Day_8/disk_monitoring_cron.sh
+++ Day_8/disk_monitoring_cron.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 THRESHOLD=80
-LOG_FILE="/var/log/disk_monitor.log"
+LOG_FILE="/root/disk_monitor.log"
 
-DISK_USAGE=$(df / | awk 'NR==2 {print $5}' | sed 's/%//')
-
-DATE=$(date "+%Y-%m-%d %H:%M:%S")
+DISK_USAGE=$(/bin/df / | /usr/bin/awk 'NR==2 {print $5}' | /usr/bin/sed 's/%//')
+DATE=$(/bin/date "+%Y-%m-%d %H:%M:%S")
 
 if [ "$DISK_USAGE" -gt "$THRESHOLD" ]; then
-  echo "$DATE ALERT: Disk usage is ${DISK_USAGE}% (Threshold: ${THRESHOLD}%)" >> $LOG_FILE
+  echo "$DATE ALERT: Disk usage is ${DISK_USAGE}% (Threshold: ${THRESHOLD}%)" >> "$LOG_FILE"
 else
-  echo "$DATE Disk usage is normal: ${DISK_USAGE}%" >> $LOG_FILE
+  echo "$DATE Disk usage is normal: ${DISK_USAGE}%" >> "$LOG_FILE"
 fi
-
